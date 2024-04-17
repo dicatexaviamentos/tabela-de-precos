c1d2b41d99937483d06a0140df5a6410741026b7
dicatexaviamentos <166443417+dicatexaviamentos@users.noreply.github.com>
2024-04-17 15:33:47 -0300

    update table

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function showContent() {
     document.querySelector('.content-container').classList.remove('loaded');
 }
 const ignorarPalavras = [
-    "2ª", "SEGUNDA", "2º", "KIT", "*",
-    "BONES", "CONE", "BORRACHA", "BEHA", "CARNEIRA",
-    "CORANTE", "FLAVINA", "ESTAMPA", "100%", "INDUSTRIALIZACAO",
-    "REBOBINAR", "COSTURA", "TRICHE", "ALVEJADO", "ALFA FOSCA",
-    "ALFA MISTA", "AMOSTRA", "FOTOCROMIA", "GLOW",
-    "TEMPERATURA", "LINHA BORDAR", "CORES", "ENFRALDADO", "WACKY", "GORGURAO",
-    "22MM", "NW 1.40LG", "METALIC", "CONFECCAO", "DIVERSOS",
+    "2ª", "SEGUNDA", "2º", "KIT", "EJETOR", "EMPUNHADEIRA", "CURVA", "VISEIRA", "TRICOLOR",
+    "BONES", "CONE", "BORRACHA", "BEHA", "CARNEIRA", "CORTE", "EXPANDIDA", "GL100", "5 FAIXAS",
+    "CORANTE", "FLAVINA", "ESTAMPA", "100%", "INDUSTRIALIZACAO", "INFANTIL", "18 FIOS",
+    "REBOBINAR", "COSTURA", "TRICHE", "ALVEJADO", "ALFA FOSCA", "ABA BRANCA", "TINGIR",
+    "ALFA MISTA", "AMOSTRA", "FOTOCROMIA", "GLOW", "A69 / G32", "NORMAL BRANCA", "PUXADO",
+    "TEMPERATURA", "LINHA BORDAR", "CORES", "ENFRALDADO", "WACKY", "GORGURAO", "VIES NOR. 50MM",
+    "22MM", "NW 1.40LG", "METALIC", "CONFECCAO", "DIVERSOS", "ABA NORMAL", "COMPRESSÃO", "BICOLOR",
 ];
 function palavraDeveSerIgnorada(palavra) {
     return ignorarPalavras.some(palavraIgnorada => palavra.includes(palavraIgnorada));
@@ -28,7 +28,7 @@ function ajustarNomeProduto(nomeProduto) {
     } else if (nomeProduto.includes('AGULHA P/ RETA CABO FINO')) {
         return 'AGULHA P/ RETA CABO FINO';
     } else if (nomeProduto.includes('AGULHA P/ RETA CABO GROS')) {
-        return 'AGULHA P/ RETA CABO GROS';
+        return 'AGULHA P/ RETA CABO GROSSO';
     } else if (nomeProduto.includes('AGULHA P/BORDAR DB X K5')) {
         return 'AGULHA P/BORDAR DB X K5';
     } else if (nomeProduto.includes('AGULHA PONTA LANÇA 134 PCL')) {
@@ -55,15 +55,46 @@ function ajustarNomeProduto(nomeProduto) {
         return 'FECHO DE CONTATO 50MM BRANCO E PRETO';
     } else if (nomeProduto.includes('FECHO DE CONTATO 25MM')) {
         return 'FECHO DE CONTATO 25MM BRANCO E PRETO';
-    } else if (nomeProduto.includes('FECHO DE CONTATO 20MM')) {
-        return 'FECHO DE CONTATO 20MM BRANCO E PRETO';
     } else if (nomeProduto.includes('FECHO DE CONTATO 16MM')) {
         return 'FECHO DE CONTATO 16MM BRANCO E PRETO';
     } else if (nomeProduto.includes('FECHO DE CONTATO 100MM')) {
         return 'FECHO DE CONTATO 100MM BRANCO E PRETO';
     } else if (nomeProduto.includes('E.V.A 3.5MM D40')) {
         return 'E.V.A 3.5MM D40 (VERIFICAR CORES)';
+    } else if (nomeProduto.includes('AGULHA PONTA LANÇA INF. 134 LR')) {
+        return 'AGULHA PONTA LANÇA INF. 134 LR';
+    } else if (nomeProduto.includes('ABA ALFA')) {
+        return 'ABA ALFA (VERIFICAR MODELOS E VALORES)';
+    } else if (nomeProduto.includes('AGULHA B 64 GB')) {
+        return 'AGULHA B 64 GB INTERLOQUE';
+    } else if (nomeProduto.includes('AGULHA DO X 558')) {
+        return 'AGULHA DO X 558';
+    } else if (nomeProduto.includes('BOTAO PINO ALUM.')) {
+        return 'BOTAO PINO ALUM. CORES';
+    } else if (nomeProduto.includes('EL. CARRETEL 100M ALFA 7MM 0100')) {
+        return 'EL. CARRETEL 100M ALFA 7MM BRANCO';
+    } else if (nomeProduto.includes('FIVELA PASSANTE 18MM')) {
+        return 'FIVELA PASSANTE 18MM';
+    } else if (nomeProduto.includes('ILHOS OVAL NIQUELADO')) {
+        return 'ILHOS OVAL 250 UN';
+    } else if (nomeProduto.includes('ILHOS 108/60 FIVELA')) {
+        return 'ILHOS 108/60 FIVELA';
+    } else if (nomeProduto.includes('ILHOS 45 COM ARRUELA')) {
+        return 'ILHOS 45 COM ARRUELA';
+    } else if (nomeProduto.includes('LINHA BONDED NYL 40    ALFA 250G')) {
+        return 'LINHA BONDED NYL ALFA 250G';
+    } else if (nomeProduto.includes('LINHA BONDED NYL 40    ALFA 80G')) {
+        return 'LINHA BONDED NYL ALFA 80G';
+    } else if (nomeProduto.includes('LINHA BONDED NYL 60    ALFA 80G')) {
+        return 'LINHA BONDED NYL ALFA 80G';
+    } else if (nomeProduto.includes('LINHA NYLON 40 ALFA 250G')) {
+        return 'LINHA NYLON ALFA 250G';
+    } else if (nomeProduto.includes('LINHA NYLON 40 ALFA 80G')) {
+        return 'LINHA NYLON ALFA 80G';
+    } else if (nomeProduto.includes('REGULADOR PANDA 1 PINO')) {
+        return 'REGULADOR PANDA 1 PINO (VERIFICAR CORES)';
     } else {
+        
         return nomeProduto;
     }
 }
@@ -140,9 +171,9 @@ window.onload = function () {
                                             if (family === 'LINHAS' && subFamily === 'OVERLOQUE') {
                                                 const newProductName = (productName) => {
                                                     if (productName.includes('LINHA 100G OVER ALFA') && !productName.includes('0100') && !productName.includes('9900')) {
-                                                        return 'LINHA 100G OVER ALFA COLOR';
+                                                        return 'LINHA 100G OVER ALFA CORES';
                                                     } else if (productName.includes('LINHA 500G OVER ALFA') && !productName.includes('0100') && !productName.includes('9900')) {
-                                                        return 'LINHA 500G OVER ALFA COLOR';
+                                                        return 'LINHA 500G OVER ALFA CORES';
                                                     }
                                                     return productName;
                                                 };
@@ -157,7 +188,7 @@ window.onload = function () {
                                                 };
                                                 existingProduct.productName = newProductName(existingProduct.productName);
                                             }
-                                            if (family === 'VIES NORMAL' && (subFamily === '14 MM' || subFamily === '16 MM' || subFamily === '20 MM' || subFamily === '25 MM' || subFamily === '30 MM')) {
+                                            if (family === 'VIES NORMAL' && (subFamily === '14 MM' || subFamily === '16 MM' || subFamily === '20 MM' || subFamily === '25 MM' || subFamily === '27 MM'|| subFamily === '30 MM')) {
                                                 const newProductName = (productName) => {
                                                     if (productName.includes('VIES NOR.') && !productName.includes('BRANCO') && !productName.includes('PRETO')) {
                                                         return `VIES NOR. ${subFamily} CORES`;
@@ -169,24 +200,92 @@ window.onload = function () {
                                             if (family === 'SUTACHE' && (subFamily === 'CORES')) {
                                                 const newProductName = (productName) => {
                                                     if (productName.includes('SUTACHE ALFA') && !productName.includes('0100') && !productName.includes('9900')) {
-                                                        return `SUTACHE ALFA ${subFamily}`;
+                                                        return `SUTACHE ALFA ${subFamily} (VERIFICAR CORES)`;
                                                     }
                                                     return productName;
                                                 };
                                                 existingProduct.productName = newProductName(existingProduct.productName);
                                             }
+
+                                            if (family === 'LINHAS' && (subFamily === '10000M')) {
+                                                const newProductName = (productName) => {
+                                                    if (productName.includes('LINHA ALFA 120 10000M') && !productName.includes('WHITE')) {
+                                                        return `LINHA ALFA 120 10000M`;
+                                                    }
+                                                    return productName;
+                                                };
+                                                existingProduct.productName = newProductName(existingProduct.productName);
+                                            }
+
+                                            if (family === 'LINHAS' && (subFamily === 'RETA')) {
+                                                const newProductName = (productName) => {
+                                                    if (productName.includes('LINHA ALFA 120 1500J') && !productName.includes('WHITE')) {
+                                                        return `LINHA ALFA 120 1500J`;
+                                                    }
+                                                    return productName;
+                                                };
+                                                existingProduct.productName = newProductName(existingProduct.productName);
+                                            }
+                                            if (family === 'LINHAS' && (subFamily === '5000M')) {
+                                                const newProductName = (productName) => {
+                                                    if (productName.includes('LINHA ALFA 120 5000M') && !productName.includes('WHITE')) {
+                                                        return `LINHA ALFA 120 5000M`;
+                                                    }
+                                                    return productName;
+                                                };
+                                                existingProduct.productName = newProductName(existingProduct.productName);
+                                            }
+                                            if (family === 'CADARÇO' && (subFamily === 'POLIESTER')) {
+                                                const newProductName = (productName) => {
+                                                    if (productName.includes('CADARCO ALFA 100M') && !productName.includes('0100') && !productName.includes('9900') && !productName.includes('MESCLA')) {
+                                                        return `CADARCO ALFA 100M CORES`;
+                                                    }
+                                                    return productName;
+                                                };
+                                                existingProduct.productName = newProductName(existingProduct.productName);
+                                            }
+                                            if (family === 'CORDÕES' && (subFamily === 'PP')) {
+                                                const newProductName = (productName) => {
+                                                    if (productName.includes('CORDAO CHAPEU 4/2') && !productName.includes('BRANCO')) {
+                                                        return `CORDAO CHAPEU 4/2 CORES`;
+                                                    }
+                                                    return productName;
+                                                };
+                                                existingProduct.productName = newProductName(existingProduct.productName);
+                                            }
+                                            if (family === 'VELCRO' && (subFamily === '20 MM')) {
+                                                const newProductName = (productName) => {
+                                                    if (productName.includes('FECHO DE CONTATO 20MM') && !productName.includes('FEMEA')) {
+                                                        return `FECHO DE CONTATO 20MM BRANCO E PRETO`;
+                                                    }
+                                                    return productName;
+                                                };
+                                                existingProduct.productName = newProductName(existingProduct.productName);
+                                            }
+
+
                                         }
                                     }
                                 });
-                                const tableBody = document.querySelector('#csvTable tbody');
+                                const container = document.querySelector('.content-container');
+
                                 for (const familySubfamily in groupedProducts) {
                                     const productGroup = groupedProducts[familySubfamily];
+
+                                    // Criar uma nova tabela para o grupo de família/subfamília
+                                    const table = document.createElement('table');
+                                    const tbody = document.createElement('tbody');
+
                                     // Adicionar uma linha para a família/subfamília
                                     const familySubfamilyRow = document.createElement('tr');
-                                    familySubfamilyRow.innerHTML = `
-                                        <td colspan="4" class="family-subfamily">${productGroup.familySubfamily}</td>
-                                    `;
-                                    tableBody.appendChild(familySubfamilyRow);
+                                    const familySubfamilyCell = document.createElement('td');
+                                    familySubfamilyCell.setAttribute('colspan', '4');
+                                    familySubfamilyCell.textContent = productGroup.familySubfamily;
+                                    familySubfamilyCell.classList.add('family-subfamily'); // Adiciona a classe .family-subfamily à célula
+                                    familySubfamilyRow.appendChild(familySubfamilyCell);
+
+                                    tbody.appendChild(familySubfamilyRow);
+
                                     // Adicionar o cabeçalho Produto Unid Padrão Distribuidor
                                     const headerRow = document.createElement('tr');
                                     headerRow.innerHTML = `
@@ -195,14 +294,15 @@ window.onload = function () {
                                         <th>Padrão</th>
                                         <th>Distribuidor</th>
                                     `;
-                                    tableBody.appendChild(headerRow);
-                                    // Adicionar linhas para cada produto na família/subfamília
+                                    tbody.appendChild(headerRow);
+
                                     // Adicionar linhas para cada produto na família/subfamília
                                     productGroup.products.forEach(product => {
                                         const productRow = document.createElement('tr');
                                         const distributorPrice = parseFloat(product.distributorPrice.replace(',', '.'));
                                         let productName = product.productName; // mantém o nome original do produto
                                         let productUnit = productGroup.unitOfMeasure; // mantém a unidade de medida padrão
+
                                         // Verifica se o nome do produto contém a palavra-chave
                                         if (productName.includes('EL. MT ALFA')) {
                                             productUnit = 'METROS'; // Atualiza a unidade de medida
@@ -211,15 +311,26 @@ window.onload = function () {
                                         } else if (productName.includes('LINHA NYLON 0,18MM')) {
                                             productUnit = 'KILOS'; // Atualiza a unidade de medida
                                         }
+
                                         productRow.innerHTML = `
-        <td>${productName}</td>
-        <td>${productUnit}</td>
-        <td>${parseFloat(product.productPrice.replace(',', '.')).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 })}</td>
-        <td>${isNaN(distributorPrice) ? '---------' : distributorPrice.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 })}</td>
-    `;
-                                        tableBody.appendChild(productRow);
+                                            <td>${productName}</td>
+                                            <td>${productUnit}</td>
+                                            <td>${parseFloat(product.productPrice.replace(',', '.')).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 })}</td>
+                                            <td>${isNaN(distributorPrice) ? '---------' : distributorPrice.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 })}</td>
+                                        `;
+                                        tbody.appendChild(productRow);
                                     });
+
+                                    // Adicionar o tbody à tabela
+                                    table.appendChild(tbody);
+
+                                    // Adicionar a tabela ao contêiner
+                                    container.appendChild(table);
+
+                                    // Adicionar uma quebra de linha após cada grupo
+                                    container.appendChild(document.createElement('br'));
                                 }
+
                                 // Agora que os dados estão prontos, oculte a animação de carregamento e mostre a tabela
                                 showContent();
                             })
